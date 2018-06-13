1053ee4af31b0954a88454a48b71728785a0d10f
rubisetcie <rubisetcie@gmail.com>
2018-06-13 13:43:58 +0200

    MAJ de l'algorithme

--- 8. Programmation/Arduino/Arduino.ino
+++ 8. Programmation/Arduino/Arduino.ino
@@ -27,18 +27,22 @@ struct Point
 {
   int x;
   int y;
+
+  int heuristique;
 };
 
 // Déclaration des variables :
 Point points[12];     // Les points atteignables et leur coordonnées
+
 Point* pointCourant;  // Pointeur vers le point sur lequel le robot se trouve
+Point* pointDest;     // Pointeur vers le point sur lequel le robot doit aller
 
 // Fonction "setup" exécutée au lancement :
 void setup()
 {
-  // On initialise les moteurs et le moniteur série :
+  // On initialise les moteurs :
   Motor.begin(I2C_ADDRESS);
-  Serial.begin(9600);
+  //Serial.begin(9600);
 
   // On commence à avancer :
   avance();
@@ -57,10 +61,12 @@ void loop()
   lectureCapteurs(cpt_A, cpt_B, cpt_C, cpt_D);
   
   // On corrige la trajectoire si les capteurs B ou C détectent du noir :
-  if (cpt_B == HIGH)
+  if (cpt_B == HIGH && cpt_C == LOW)
     tourneDroite(SEUIL_COR);
-  else if (cpt_C == HIGH)
+  else if (cpt_C == HIGH && cpt_B == LOW)
     tourneGauche(SEUIL_COR);
+  else if (cpt_B == HIGH && cpt_C == HIGH)
+    arrete();
 }
 
 // Fonction "lectureCapteurs" permet de lire l'état des capteurs :
@@ -84,6 +90,13 @@ void recule()
 {
   Motor.speed(MOTOR1, AR_MIN);
   Motor.speed(MOTOR2, AR_MIN); 
+}
+
+// Fonction "arrete" permet de s'arrêter :
+void arrete()
+{
+  Motor.speed(MOTOR1, 0);
+  Motor.speed(MOTOR2, 0); 
 }
 
 // Fonction "tourneDroite" permet de tourner d'un certain angle vers la droite :
@@ -110,8 +123,24 @@ void tourneGauche(unsigned int del)
   avance();
 }
 
+// Fonction "preparerCarte" permet de charger la carte dans la mémoire :
+void preparerCarte()
+{
+}
+
 // Fonction "pointSuivant" permet de se diriger vers le prochain point :
 void pointSuivant(unsigned int pt)
 {
+}
+
+// Fonction "comparePoints" compare deux heuristiques de points différents :
+int comparePoints(const Point& pA, const Point& pB)
+{
+  if (pA.heuristique < pB.heuristique)
+    return -1;
+  else if (pA.heuristique > pB.heuristique)
+    return 1;
+  else
+    return 0;
 }
 
